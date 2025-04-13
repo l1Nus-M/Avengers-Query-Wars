58d2c63b953aa8388996949031455a3424642c54
Linus Msuri <razer5854@gmail.com>
2025-04-13 19:28:08 +0300

    Users Roles and Permissions File

---
+++ user_roles.sql
@@ -0,0 +1,10 @@
+-- Users and Grant Permissions --
+/* Creating Users and Grant Permissions */
+CREATE USER '<enter_user_name>'@'localhost' IDENTIFIED BY "<enter_password>";
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON BookStoreDB.* TO '<enter_user_name>'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE ROLE 'read_only';
+GRANT SELECT ON BookStoreDB.* TO 'read_only';
+GRANT 'read_only' TO '<enter_user_name>'@'localhost';
