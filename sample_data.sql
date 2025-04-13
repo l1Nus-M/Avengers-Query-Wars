da2602771feb038c0267e80172222dd09d6fdeb1
Nontuthuzelo49 <140985037+Nontuthuzelo49@users.noreply.github.com>
2025-04-13 19:11:04 +0200

    Updated sample_data.sql

--- sample_data.sql
+++ sample_data.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
--- Host: localhost    Database: cozychapter
+-- Host: localhost    Database: cozychapterdb
 -- ------------------------------------------------------
 -- Server version	8.0.41
 
